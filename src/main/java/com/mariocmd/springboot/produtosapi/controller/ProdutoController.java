996bb86e923ad9f606a2845629cc2ffd76c0da7d
mario <mario.correa.ti@outlook.com>
2026-01-02 17:17:03 -0300

    feat: add repository methods.

--- src/main/java/com/mariocmd/springboot/produtosapi/controller/ProdutoController.java
+++ src/main/java/com/mariocmd/springboot/produtosapi/controller/ProdutoController.java
@@ -2,10 +2,7 @@ package com.mariocmd.springboot.produtosapi.controller;
 
 import com.mariocmd.springboot.produtosapi.model.Produto;
 import com.mariocmd.springboot.produtosapi.reporitory.ProdutoRepository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -22,8 +19,26 @@ public class ProdutoController {
     @PostMapping
     public Produto salvar(@RequestBody Produto produto){
         System.out.println("Criando o produto: " + produto);
+        String uuid = UUID.randomUUID().toString();
+        produto.setId(uuid);
         produtoRepository.save(produto);
         return produto;
+    }
+
+    @GetMapping("{id}")
+    public Produto getProduto(@PathVariable String id){
+        return produtoRepository.findById(id).orElse(null);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id){
+        produtoRepository.deleteById(id);
+    }
+
+    @PutMapping("{id}")
+    public void update(@PathVariable("id") String id, @RequestBody Produto produto){
+        produto.setId(id);
+        produtoRepository.save(produto);
     }
 
 }
